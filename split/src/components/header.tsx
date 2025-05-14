065bf5e443102254c59ef8ffd1c0f689e6cc3d22
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-05-14 08:43:17 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ split/src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './ui/ModeChanger'
+
+function Header() {
+  return (
+    <div>Header
+        <ModeToggle/>
+    </div>
+  )
+}
+
+export default Header
